caf5cb4df5be23b29404cb2fc9cd791b550e8e60
Prashanth RVVNS <prashanth2142@gmail.com>
2023-08-26 21:57:59 +0530

    uploaded files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { TreeService } from './tree-service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Practice';
+  title = 'Tree View'; 
+
+  constructor(private treeService: TreeService) { 
+  }
 }
